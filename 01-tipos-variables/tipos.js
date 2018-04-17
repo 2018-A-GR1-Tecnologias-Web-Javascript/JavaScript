725d53406553ab1fcbff37fdf7d8fdd9e4a676b6
Adrian Eguez <eadepto@hotmail.com>
2018-04-17 15:07:34 -0500

    variabls

--- 01-tipos-variables/tipos.js
+++ 01-tipos-variables/tipos.js
@@ -1,8 +1,33 @@
 var nombre = "28";
 var edad = 28
 var fecha = new Date();
+var casado = false; // true
+var casa = undefined;
 
-if(nombre !== edad){
+
+
+
+
+
+
+
+
+
+var arreglo = []
+console.log(casa ? "Verdadero" : "Falso");
+if(casa){
+    console.log("Verdadero");
+} else {
+    console.log("Falso");
+}
+
+
+var carro = null;
+
+
+console.log(fecha);
+// ctrl + alt + l
+if (nombre !== edad) {
     console.log("Verdadero");
 } else {
     console.log("Falso");
